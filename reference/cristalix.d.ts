933ba12707a537b68ad8ff3aca44370c404166d1
xxDark <19853368+xxDark@users.noreply.github.com>
2020-12-28 19:50:29 +0300

    Add event names

--- reference/cristalix.d.ts
+++ reference/cristalix.d.ts
@@ -748,7 +748,7 @@ declare type CancellableRenderEvent = {
     cancelled: boolean,
 }l
 
-declare type EventAction = 'chat_send' | 'gui_overlay_render' | 'game_loop' | 'static KEY_press' | 'server_connect' | 'server_switch' | 'overlay_render' | 'game_tick_pre' | 'game_tick_post' | string;
+declare type EventAction = 'chat_send' | 'gui_overlay_render' | 'game_loop' | 'static KEY_press' | 'server_connect' | 'server_switch' | 'overlay_render' | 'game_tick_pre' | 'game_tick_post' | 'player_list_render' | 'air_bar_render' | 'hotbar_render' | 'potions_render' | 'exp_render' | 'armor_render' | 'health_render' | 'hunger_render' | string;
 declare type Listener<T> = (event: T) => void;
 
 declare class Events {
